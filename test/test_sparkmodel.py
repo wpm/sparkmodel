ff20096a9ef8503ec22e91f17eed01894915c11c
Bill McNeill <wmcneill@sparkcognition.com>
2017-11-17 18:28:32 -0600

    Unit tests
    File deletion error at the very end of unit tests

--- test/test_sparkmodel.py
+++ test/test_sparkmodel.py
@@ -6,18 +6,20 @@ from sparkmodel.main import main
 
 
 class TestCommandLine(unittest.TestCase):
-    def setUp(self):
-        self.runner = CliRunner()
+    @classmethod
+    def setUpClass(cls):
+        cls.runner = CliRunner()
 
     def test_version(self):
         result = self.runner.invoke(main, ['--version'])
         self.assertEqual(result.exit_code, 0)
 
     def test_model(self):
-        self.runner.invoke(main, ["generate", "train-data"])
-        self.runner.invoke(main, ["generate", "--n=100", "test-data"])
-        result = self.runner.invoke(main, ["train", "model", "train-data"])
-        self.assertEqual(result.exit_code, 0, msg=result.output)
-        result = self.runner.invoke(main, ["predict", "model", "test-data", "--labeled-data=test-output"])
-        self.assertEqual(result.exit_code, 0, msg=result.output)
-        self.assertRegexpMatches(result.output, "Accuracy 0.\d\d\d\d")
+        with self.runner.isolated_filesystem():
+            self.runner.invoke(main, ["generate", "train-data"])
+            self.runner.invoke(main, ["generate", "--n=100", "test-data"])
+            result = self.runner.invoke(main, ["train", "model", "train-data"])
+            self.assertEqual(result.exit_code, 0, msg=result.output)
+            result = self.runner.invoke(main, ["predict", "model", "test-data", "--labeled-data=test-output"])
+            self.assertEqual(result.exit_code, 0, msg=result.output)
+            self.assertRegexpMatches(result.output, "Accuracy 0.\d\d\d\d")

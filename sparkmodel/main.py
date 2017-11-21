861c106e0e7d8fba7425a9f0a3f6fe97f8c5d474
Bill McNeill <wmcneill@sparkcognition.com>
2017-11-21 09:35:49 -0600

    Add Logistic Regression model

--- sparkmodel/main.py
+++ sparkmodel/main.py
@@ -3,7 +3,7 @@ import logging
 import click as click
 from numpy import array
 from pyspark.ml import Pipeline, PipelineModel
-from pyspark.ml.classification import LinearSVC
+from pyspark.ml.classification import LinearSVC, LogisticRegression
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.linalg import Vectors
 from pyspark.sql import SparkSession
@@ -33,6 +33,13 @@ def train():
     pass
 
 
+def train_pipeline(model_path: str, data_path: str, estimator):
+    data = spark().read.load(data_path)
+    pipeline = Pipeline(stages=[estimator]).fit(data)
+    pipeline.save(model_path)
+    logging.info(f"""Created pipeline model in {model_path}""")
+
+
 @click.command(short_help="support vector machine")
 @click.argument("model_path", metavar="MODEL")
 @click.argument("data_path", metavar="DATA")
@@ -47,19 +54,51 @@ def train():
                    "can be any real number, where Inf will make all predictions 0.0 and -Inf will make all "
                    "predictions 1.0.")
 @click.option("--tol", default=1e-6, help="the convergence tolerance for iterative algorithms (>= 0).")
-def svm(model_path: str, data_path: str, aggregation_depth: int, fit_intercept: bool, max_iter: int, reg_param: float,
-        standardization: bool, threshold: float, tol: float):
+def svm(model_path: str, data_path: str,
+        aggregation_depth: int, fit_intercept: bool, max_iter: int, reg_param: float, standardization: bool,
+        threshold: float, tol: float):
     """Train a support vector machine on DATA and save it as MODEL.
     """
-    data = spark().read.load(data_path)
-    pipeline = Pipeline(stages=[
-        LinearSVC(aggregationDepth=aggregation_depth, fitIntercept=fit_intercept, maxIter=max_iter, regParam=reg_param,
-                  standardization=standardization, threshold=threshold, tol=tol)]).fit(data)
-    pipeline.save(model_path)
-    logging.info(f"""Created model in {model_path}""")
+    svm_estimator = LinearSVC(aggregationDepth=aggregation_depth, fitIntercept=fit_intercept, maxIter=max_iter,
+                              regParam=reg_param, standardization=standardization, threshold=threshold, tol=tol)
+    return train_pipeline(model_path, data_path, svm_estimator)
 
 
 train.add_command(svm)
+
+
+@click.command(short_help="logistic regression")
+@click.argument("model_path", metavar="MODEL")
+@click.argument("data_path", metavar="DATA")
+@click.option("--max-iter", default=100, help="max number of iterations (>= 0).")
+@click.option("--reg-param", default=0.0, help="regularization parameter (>= 0).")
+@click.option("--elastic-net-param", default=0.0,
+              help="the ElasticNet mixing parameter, in range [0, 1]. For alpha = 0, the penalty is an L2 penalty. "
+                   "For alpha = 1, it is an L1 penalty.")
+@click.option("--tol", default=1e-6, help="the convergence tolerance for iterative algorithms (>= 0).")
+@click.option("--threshold", default=0.5,
+              help="Threshold in binary classification prediction, in range [0, 1]. If threshold and thresholds are "
+                   "both set, they must match.e.g. if threshold is p, then thresholds must be equal to [1-p, p].")
+@click.option("--standardization", default=True,
+              help="whether to standardize the training features before fitting the model.")
+@click.option("--aggregation-depth", default=2, help="suggested depth for treeAggregate (>= 2).")
+@click.option("--family", type=click.Choice(["auto", "binomial", "multinomial"]), default="auto",
+              help="The name of family which is a description of the label distribution to be used in the model. "
+                   "Supported options: auto, binomial, multinomial")
+def logistic_regression(model_path: str, data_path: str,
+                        max_iter: int, reg_param: float, elastic_net_param: float, tol: float, threshold: float,
+                        standardization: bool, aggregation_depth: int, family: str):
+    """Train a logistic regression predictor on DATA and save it as MODEL.
+    """
+    logistic_regression_estimator = LogisticRegression(maxIter=max_iter, regParam=reg_param,
+                                                       elasticNetParam=elastic_net_param,
+                                                       tol=tol,
+                                                       threshold=threshold, standardization=standardization,
+                                                       aggregationDepth=aggregation_depth, family=family)
+    return train_pipeline(model_path, data_path, logistic_regression_estimator)
+
+
+train.add_command(logistic_regression, name="logistic-regression")
 
 
 @click.command(short_help="Predict labels")
@@ -95,11 +134,6 @@ def generate(n: int, output_path: str):
     data.write.save(output_path)
 
 
-main.add_command(train)
-main.add_command(predict)
-main.add_command(generate)
-
-
 def generate_data(n: int) -> (array, array):
     """
     Generate random training data
@@ -112,3 +146,8 @@ def generate_data(n: int) -> (array, array):
     logging.info(f"Generate {n} data points.")
     x, y = make_blobs(n, centers=[(-1, -1), (1, 1)])
     return x, y
+
+
+main.add_command(train)
+main.add_command(predict)
+main.add_command(generate)
